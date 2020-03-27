d7729ad98ad24fe3cec24b652a7be4b9a80281a5
ohkhaila <khaila.jones@lessannoyingcrm.com>
2020-03-27 11:45:47 -0500

    Fetching Planetary Data functional. Assignment 5 complete!

--- script.js
+++ script.js
@@ -12,7 +12,24 @@ be numbers.
 */
 window.addEventListener("load", function(){
    let getForm = document.querySelector("form");
-  
+   let getMissionTargetDiv=document.getElementById("missionTarget");
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+      //access the JSON in the response
+      response.json().then(function(json){
+         const getMissionTargetDiv=document.getElementById("missionTarget");
+         getMissionTargetDiv.innerHTML=
+         `<h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${json[2].name}</li>
+               <li>Diameter: ${json[2].diameter}</li>
+               <li>Star: ${json[2].star}</li>
+               <li>Distance from Earth: ${json[2].distance}</li>
+               <li>Number of Moons: ${json[2].moons}</li>
+            </ol>
+            <img src="${json[2].image}"></img>
+         `;   
+      })
+   });
    getForm.addEventListener("submit", function(){
       let pilotNameInput=document.querySelector("input[name=pilotName]");
       let copilotNameInput=document.querySelector("input[name=copilotName]");
@@ -69,19 +86,11 @@ window.addEventListener("load", function(){
          updateLaunchStatus.style.color="green";
          event.preventDefault();
       }
+
+            
+
    });
 });
 
 
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
